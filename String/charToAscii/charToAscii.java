6a16d7841da495788343a1dd37aa57777b34b315
YoungseoYoo <67851701+YoungseoYoo@users.noreply.github.com>
2020-08-09 10:01:32 +0900

    Add files via upload

---
+++ String/charToAscii/charToAscii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        
+        // check
+        int strLen = s.length();
+        if ((strLen == 4) || (strLen == 6)){
+            for (int i = 0; i < strLen; i++){
+                char a = s.charAt(i);
+                int asc = (int)a;
+                if ((65 <= a) && (a <= 90)){
+                    answer = false;
+                    break;
+                }
+                if ((97 <= a) && (a <= 122)){
+                    answer = false;
+                    break;
+                }
+            }
+        }
+        else answer = false;
+        
+        return answer;
+    }
+}
