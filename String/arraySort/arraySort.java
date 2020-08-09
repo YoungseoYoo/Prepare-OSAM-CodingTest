271c15f25b35673bf31c5ff8f9172096821e7af8
YoungseoYoo <67851701+YoungseoYoo@users.noreply.github.com>
2020-08-09 09:30:16 +0900

    Update arraySort.java

--- String/arraySort/arraySort.java
+++ String/arraySort/arraySort.java
@@ -1,36 +1,31 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.ArrayList; // ---------------------------------- for ArrayList
+import java.util.Collections; // -------------------------------- for Sort
 
 class Solution {
     public long solution(long n) {
         long answer = 0;
         
         // change data's form
-        String s = String.valueOf(n);
+        String s = String.valueOf(n); // ------------------------- long to String
         ArrayList<Integer> list = new ArrayList<>();
         for (int i = 0; i < s.length(); i++){
-            int ipd = Integer.parseInt(s.substring(i, i+1));
+            int ipd = Integer.parseInt(s.substring(i, i+1)); // -- String to int
             list.add(ipd);
         }
         
         // asc sort
-        // Collections.sort(list);
+        // Collections.sort(list); // ---------------------------- asc sort
         // desc sort
-        Collections.sort(list, Collections.reverseOrder());
+        Collections.sort(list, Collections.reverseOrder()); // --- desc sort
         
         // make String result
         String data = "";
         for (Integer re : list){
-            data += Integer.toString(re);
+            data += Integer.toString(re); // ---------------------- int to String
         }
         
         // make Long result
-        answer = Long.parseLong(data);
-        
-        // for test
-        // System.out.println(data);
-
-        
+        answer = Long.parseLong(data); // ------------------------- String to long       
         
         return answer;
     }
