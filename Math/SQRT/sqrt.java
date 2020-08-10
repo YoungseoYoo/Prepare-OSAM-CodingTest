bd7c5f8130828461ca9445368464bf8147378be6
YoungseoYoo <67851701+YoungseoYoo@users.noreply.github.com>
2020-08-10 23:04:48 +0900

    Add files via upload

---
+++ Math/SQRT/sqrt.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        double dResult = 0.0;
+        int iResult = 0;
+        dResult = Math.sqrt(n);
+        iResult = (int)dResult;
+        
+         check
+        if (dResult - (double)iResult != 0.0){
+            answer = -1;
+        }else{
+            answer = (long) Math.pow(iResult + 1, 2);
+        }
+        
+        return answer;
+    }
+}
